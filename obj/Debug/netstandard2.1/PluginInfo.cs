841ee9caac46f106f567743e05413859a87b3508
Wexop <mattheurtin@gmail.com>
2024-09-21 12:28:41 +0200

    ignore

--- obj/Debug/netstandard2.1/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace RCCars
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "RCCars";
-        public const string PLUGIN_NAME = "RCCars";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
