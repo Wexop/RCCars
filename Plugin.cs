dd886e93ca1d5de94fe92744948ca5169377e5cd
Wexop <mattheurtin@gmail.com>
2024-09-22 20:06:52 +0200

    v1.0.2

--- Plugin.cs
+++ Plugin.cs
@@ -18,7 +18,7 @@ namespace RCCars
     {
         private const string GUID = "wexop.rc_cars";
         private const string NAME = "RCCars";
-        private const string VERSION = "1.0.1";
+        private const string VERSION = "1.0.2";
 
         public static RCCarsPlugin instance;
 
--- Thunderstore/CHANGELOG.md
+++ Thunderstore/CHANGELOG.md
@@ -1,5 +1,12 @@
 # Changelog
 
+### v1.0.2
+
+- Add RC Cruiser Car
+- Add RC Bomb Car
+- Car explosion now affect enemies and other cars
+- Fix an issue where you could grab an item already grabbed
+
 ### v1.0.1
 
 - Disable driving on company building do to an issue
